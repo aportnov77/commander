e2ab7a914d8f3a704c7b58fefc186c8f3676d69d
Jildert Miedema <jildert@pimenta.nl>
2014-08-04 18:43:55 +0200

    check for injection

--- src/JildertMiedema/Commander/Silex/CommanderTrait.php
+++ src/JildertMiedema/Commander/Silex/CommanderTrait.php
@@ -1,10 +1,15 @@
 <?php
 namespace JildertMiedema\Commander\Silex;
 
+use Silex\Application;
+
 trait CommanderTrait 
 {
 
     protected function execute($command, array $input = null, array $decorators = []) {
+        if (!isset($this->app) || !($this->app instanceof Application)) {
+            throw new \Exception(sprintf("Silex\Application needs to injected in '%s::\$app'", get_called_class()));
+        }
         $input = $input ?: $this->app['request']->query->all();
 
         $manager = $this->app['commander.manager'];
